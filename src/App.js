6f5cd2968907b2b9a20591cfaad055129c45009f
pdd2498 <136301852+pdd2498@users.noreply.github.com>
2024-03-14 12:20:54 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import Itrms from './Itrms';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 function App() {
   const [data , setData] = useState("");
@@ -16,15 +16,50 @@ function App() {
     console.log(mapData);
   }
 
-  
+  const exp = useRef(0);
+  console.log(exp);
+  let a = 1;
+
+  const loding = ()=>{
+    let ans = document.getElementById('ans');
+    let comp = document.getElementById('complite');
+    const s = setInterval(() => {
+      a += 1;
+      
+      ans.innerHTML = `${a}%`;
+      exp.current.setAttribute('aria-valuenow', a);
+      exp.current.style.width = `${a*5}px`;
+      // setLoding(a);
+      if(a === 100){
+        comp.innerText = "Complite"
+        updatData();
+        clearInterval(s);
+     }
+      console.log(a);
+    }, 100);
+  }
+
+  // loding()
   return (
     <div className="App">
       <header className="App-header">
+        <div style={{
+          height: "20px",
+          width : "500px",
+        }}>
+            <div ref={exp} aria-valuemin={0} aria-valuemax={100} style={{
+              height: "20px",
+              backgroundColor:"green",
+              
+            }}></div>
+        </div>
+        <p id='complite'>Click on Add item button then loding is start </p>
+        <h1 id='ans'></h1>
         <div>
           <h2>Grocery Bud</h2>
           <div>
             <input type="text" value={data} onChange={(e) => {setData(e.target.value)}} />
-            <button onClick={updatData}>Add item</button>
+            <button onClick={loding}>Add item</button>
           </div>
           {
             mapData.map((e, index) => < Itrms value ={e} idx = {index} mapData = {setMapData} />)
